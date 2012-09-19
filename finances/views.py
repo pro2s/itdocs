fd87212e9d01f12a03ff5754a5bb1d5a70d11b62
protasov.s.s@gmail.com <protasov.s.s@gmail.com@a43743f8-f3a2-4b77-92e0-78748224bbd3>
2012-09-19 17:36:03 +0000

    output Payments

--- finances/views.py
+++ finances/views.py
@@ -1,1 +1,31 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.template import Context, loader
+from finances.models import Payment
+from django.shortcuts import render_to_response
+from django.http import Http404
+
+def index(request):
+    return HttpResponse(u"<a href='/payments/'>Платежи</a> ")
+
+def payments(reqyest):
+    payments_list = Payment.objects.all() #.order_by('-pub_date')[:5]
+    t = loader.get_template('payments/index.html')
+    c = Context({
+        'payments_list': payments_list,
+    })
+    return HttpResponse(t.render(c))
+    
+def payments_print(request, payment_id):
+    try:
+        p = Payment.objects.get(pk=payment_id)
+    except Payment.DoesNotExist:
+        raise Http404
+    return render_to_response('payments/print.html', {'payment': p})
+    
+def payments_edit(request, payment_id):
+    try:
+        p = Payment.objects.get(pk=payment_id)
+    except Payment.DoesNotExist:
+        raise Http404
+    return render_to_response('payments/print.html', {'payment': p})
--- settings.py
+++ settings.py
@@ -118,9 +118,11 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'itdocs.urls'
 
 TEMPLATE_DIRS = (
+    './templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    
 )
 
 INSTALLED_APPS = (
