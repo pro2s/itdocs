63eddb1adf58c6ebe3e8a20bcdb18c3ea339dae3
protasov.s.s@gmail.com <protasov.s.s@gmail.com@a43743f8-f3a2-4b77-92e0-78748224bbd3>
2012-10-01 13:06:34 +0000

    add plan functions

--- finances/admin.py
+++ finances/admin.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django import forms
-from finances.models import Company, CompanyInfo, ContractType, Contract, InvoiceType, Invoice, RecieptType, Payment
+from finances.models import Company, CompanyInfo, ContractType, Contract, InvoiceType, Invoice, RecieptType, Payment, Currency, PlanItem, Plan
 
-class PaymentAdminForm(forms.ModelForm):
-  class Meta:
-    model = Payment
-
-#  def __init__(self, *args, **kwargs):
+#class PaymentAdminForm(forms.ModelForm):
+#def __init__(self, *args, **kwargs):
 #    super(PaymentAdminForm, self).__init__(*args, **kwargs)
 #    if (self.instance.contract is None):
 #        self.fields['invoices'].queryset = Invoice.objects.all()
 #    else:
 #        self.fields['invoices'].queryset = Invoice.objects.filter(contract=self.instance.contract)
 
+class PlanItemsInline(admin.TabularInline):
+    model = Plan.items.through
+
+class PlanAdmin(admin.ModelAdmin):
+    inlines = [
+        PlanItemsInline,
+    ]
+    exclude = ('items',)
+
+    
 class PaymentInvoicesInline(admin.TabularInline):
     model = Payment.invoices.through
     
 class PaymentAdmin(admin.ModelAdmin):
-    form = PaymentAdminForm
+#    form = PaymentAdminForm
 #    filter_horizontal = ('invoices',)
     inlines = [
         PaymentInvoicesInline,
@@ -36,6 +43,10 @@ admin.site.register(InvoiceType)
 admin.site.register(Invoice)
 admin.site.register(RecieptType)
 admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(PlanItem)
+admin.site.register(Currency)
+
   
 
 
